d3db97566ed140e8e90a2cc1e8db3411dc341904
Raji Elijah <rajielijah@gmail.com>
2022-04-27 14:34:00 +0100

    creating dev branch

--- lib/presentation/login/login.dart
+++ lib/presentation/login/login.dart
@@ -3,9 +3,16 @@ import 'package:onecareprovider/presentation/resources/strings_manager.dart';
 import 'package:onecareprovider/presentation/resources/values_manager.dart';
 import 'package:onecareprovider/sizes.dart';
 
-class LoginView extends StatelessWidget {
+class LoginView extends StatefulWidget {
   const LoginView({Key? key}) : super(key: key);
+
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
   final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     var sHeight = MediaQuery.of(context).size.height;
@@ -14,6 +21,7 @@ class LoginView extends StatelessWidget {
       body: _contentWidget(sHeight),
     );
   }
+
   Widget _contentWidget(double? sHeight) {
     return Container(
       padding: EdgeInsets.only(top: resHeight(AppPadding.p20, sHeight)),
