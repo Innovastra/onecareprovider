4b73c0344e4dd33f74fa210ac36b774002c1b599
Raji Elijah <rajielijah@gmail.com>
2022-04-27 12:58:18 +0100

    setting mediaQuery up

---
+++ lib/presentation/login/login.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:onecareprovider/presentation/resources/strings_manager.dart';
+import 'package:onecareprovider/presentation/resources/values_manager.dart';
+import 'package:onecareprovider/sizes.dart';
+
+class LoginView extends StatelessWidget {
+  const LoginView({Key? key}) : super(key: key);
+  final _formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    var sHeight = MediaQuery.of(context).size.height;
+    var sWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: _contentWidget(sHeight),
+    );
+  }
+  Widget _contentWidget(double? sHeight) {
+    return Container(
+      padding: EdgeInsets.only(top: resHeight(AppPadding.p20, sHeight)),
+      child: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              SizedBox(height: resHeight(20, sHeight),),
+              TextFormField(),
+              SizedBox(height: resHeight(40, sHeight),
+                child: ElevatedButton(onPressed: (){}, child: const Text(AppStrings.login),),)
+            ],
+          ),
+      )
+      ),
+    );
+  }
+}
